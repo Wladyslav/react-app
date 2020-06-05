f1356d72c4d83e56fd90c3e323f808b17643a503
unknown <vladwar1994@gmail.com>
2020-06-05 19:37:18 +0200

    Updates

---
+++ precache-manifest.8955638b1e14b1ba58bf6eb632f44f37.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6edf3ab0629ff846b2e9224f0e8d133e",
+    "url": "/react-app/index.html"
+  },
+  {
+    "revision": "861f69bb1ae93d9b2e19",
+    "url": "/react-app/static/css/main.a839009d.chunk.css"
+  },
+  {
+    "revision": "84f5e4f39b9a14b3e588",
+    "url": "/react-app/static/js/2.27e94a00.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-app/static/js/2.27e94a00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "861f69bb1ae93d9b2e19",
+    "url": "/react-app/static/js/main.b93f4585.chunk.js"
+  },
+  {
+    "revision": "f836dca652dae7b1693f",
+    "url": "/react-app/static/js/runtime-main.f77147b9.js"
+  },
+  {
+    "revision": "c7352947e7c5c92f63659ba5b81aae63",
+    "url": "/react-app/static/media/icon-erector.c7352947.svg"
+  },
+  {
+    "revision": "c672b0c5bfe5f0e2b11c328450e5e781",
+    "url": "/react-app/static/media/icon-gilotin.c672b0c5.svg"
+  },
+  {
+    "revision": "223225abb5286bb581813f9651974ef0",
+    "url": "/react-app/static/media/icon-materials.223225ab.svg"
+  },
+  {
+    "revision": "7a9eb5a7884de401063a03f720b08762",
+    "url": "/react-app/static/media/icon-printers.7a9eb5a7.svg"
+  },
+  {
+    "revision": "b7da92529991e4ea375d91c84141b76a",
+    "url": "/react-app/static/media/kyoceram2040dn.b7da9252.png"
+  },
+  {
+    "revision": "e5f2773daa6f17a4419378deb73ef13f",
+    "url": "/react-app/static/media/kyocerap3060dn.e5f2773d.jpg"
+  },
+  {
+    "revision": "2d003aae383d238fb2cd5a718102ade7",
+    "url": "/react-app/static/media/printer.2d003aae.png"
+  }
+]);
